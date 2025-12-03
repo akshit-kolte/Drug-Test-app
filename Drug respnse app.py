5703c16073d183a456c6f5c07f80de797ae0cbfc
akshit-kolte <akshitkolte9026@gmail.com>
2025-12-03 23:03:14 +0530

    Update Drug respnse app.py

--- Drug respnse app.py
+++ Drug respnse app.py
@@ -11,14 +11,15 @@ st.title("Drug Response app")
 drug_dosage_mg = st.number_input("Drug Dosage (mg)",min_value=0.0)
 systolic_blood_pressure_mmHg = st.number_input("Systolic Blood Pressure (mmHg)",min_value=0.0)
 heart_rate_BPM = st.number_input("Heart Rate (BPM)",min_value=0.0)
-liver_toxicity_index_U/L = st.number_input("Liver Toxicity Index (U/L)",min_value=0.0)
-blood_glucose_level_mg/dL = st.number_input("Blood Glucose Level (mg/dL)",min_value=0.0)
+liver_toxicity_index_U_L = st.number_input("Liver Toxicity Index (U/L)",min_value=0.0)
+blood_glucose_level_mg_dL = st.number_input("Blood Glucose Level (mg/dL)",min_value=0.0)
 #Make Pred
 if st.button('Drug Response Prediction'):
 	input_data=np.array([[drug_dosage_mg, systolic_blood_pressure_mmHg, heart_rate_BPM, liver_toxicity_index_U/L, blood_glucose_level_mg/dL]])
 	prediction=model.predict(input_data)[0]
 
 	st.success(f'Predict Diabetes:{prediction:.2f}')
+
 
 
 
